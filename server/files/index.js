980510abfef1418f9b349b25c6d1f4f8d0913954
Felix Schöller <felix.schoeller47@gmail.com>
2026-04-19 13:06:26 +0200

    chore: implement builder logic

--- server/files/index.js
+++ server/files/index.js
@@ -1,109 +1,60 @@
-function displayMovie(movie) {
-    //Container
-    const container = document.createElement("article")
-    container.id = movie.imdbID
+import { ElementBuilder, ParentChildBuilder } from "./builders.js";
 
-    //Poster
-    const poster = document.createElement("img")
-    poster.src = movie.Poster
-    container.appendChild(poster)
+function appendMovie(movie, element) { 
+  //Article
+  const movieArticle = new ElementBuilder("article").id(movie.imdbID);
 
-    //Child-container
-    const childContainer = document.createElement("div")
-    childContainer.classList.add("child-container");
+  //Poster
+  const moviePoster = new ElementBuilder("img").with("src", movie.Poster);
+  moviePoster.appendTo(movieArticle.element)
 
-      //Article-header
-      const articleContainer = document.createElement("div")
-      articleContainer.classList.add("article-header");
+  //Content Container
+  const movieContent = new ElementBuilder("div").class("content-container");
+  const articleHeader = new ElementBuilder("div").class("article-header")
+  articleHeader.appendTo(movieContent.element)
+  const titleContainer = new ElementBuilder("div").class("title-container")
+  titleContainer.appendTo(articleHeader.element)
+  new ElementBuilder("h2").text(movie.Title).appendTo(titleContainer.element)
 
-        //Title-container
-        const titleContainer = document.createElement("div")
-        titleContainer.classList.add("title-container");
+  const articleSide = new ElementBuilder("div").class("article-side")
+  articleSide.appendTo(titleContainer.element)
+  const button = new ElementBuilder("button").text("Edit").listener("click", () => {
+                    editMovie(movieArticle.element.id)
+                })
+  button.appendTo(articleSide.element)
 
-            //Title
-            const title = document.createElement("h2")
-            title.textContent = movie.Title
-            titleContainer.appendChild(title)
+  movieContent.append(new ElementBuilder("p").text(`Released: ${movie.Released} | Runtime: ${movie.Runtime} min | IMDb: ${movie.imdbRating} | Metascore: ${movie.Metascore}`))  
+      .append(generateTagsElement(movie.Genres))
+      .append(new ElementBuilder("p").text(movie.Plot))
+      .append(generateListElement("Directors", movie.Directors))
+      .append(generateListElement("Writers", movie.Writers))
+      .append(generateListElement("Actors", movie.Actors))       
+  
+  movieArticle.append(movieContent) 
 
-            //Side-Container
-            const sideContainer = document.createElement("div")
-            sideContainer.classList.add("article-side");
-
-                //Edit Button
-                const edit = document.createElement("button")
-                edit.textContent = "Edit"
-                edit.addEventListener("click", () => {
-                    editMovie(container.id)
-                });
-                sideContainer.appendChild(edit)
-
-            titleContainer.appendChild(sideContainer)
-
-        articleContainer.appendChild(titleContainer)
-
-        // Info (Released, Runtime, Rating)
-        const info = document.createElement("p")
-        info.textContent = `Released: ${movie.Released} | Runtime: ${movie.Runtime} min | IMDb: ${movie.imdbRating} | Metascore: ${movie.Metascore}`
-        articleContainer.appendChild(info)
-
-      childContainer.appendChild(articleContainer)
-
-      // Genres
-      const genres = generateTagsElement(movie.Genres)
-      childContainer.appendChild(genres)
-
-      // Plot
-      const plot = document.createElement("p")
-      plot.textContent = movie.Plot
-      childContainer.appendChild(plot)
-
-      //Directors
-      const directors = generateListElement("Directors", movie.Directors)
-      childContainer.appendChild(directors)
-
-      //Writers
-      const writers = generateListElement("Writers", movie.Writers)
-      childContainer.appendChild(writers)
-      
-      //Actors
-      const actors = generateListElement("Actors", movie.Actors)
-      childContainer.appendChild(actors)
-    
-    container.appendChild(childContainer)
-
-    return container
+  movieArticle.appendTo(element)
 }
 
 function generateListElement(listTitle, list){
-    const element = document.createElement("p")
-
-    const title = document.createElement("h3");
-    title.textContent = listTitle;
-    element.appendChild(title)
-
-    const unsortedList = document.createElement("ul")
-    for(const item of list){
-        const listItem = document.createElement("li")
-        listItem.textContent = item
-        unsortedList.appendChild(listItem)
-    }
-
-    element.appendChild(unsortedList)
-
-    return element
+  return new ElementBuilder("div")
+    .append(new ElementBuilder("h3").text(listTitle))
+    .append(
+      new ParentChildBuilder("ul", "li").items(list)
+    );
 }
 
 function generateTagsElement(list){
-    const element = document.createElement("div")
+  const container = new ElementBuilder("div");
 
-    for(const item of list){
-        const span = document.createElement("span")
-        span.textContent = item
-        span.classList.add("genre-tag");
-        element.appendChild(span)
-    }
+  for (const item of list) {
+    container.append(
+      new ElementBuilder("span")
+        .class("genre-tag")
+        .text(item)
+    );
+  }
 
-    return element
+  return container;
 }
 
 function editMovie(id){
@@ -117,7 +68,7 @@ window.onload = function () {
         if (xhr.status == 200) {
             const movies = JSON.parse(xhr.responseText)
             for(const movie of movies){
-                bodyElement.append(displayMovie(movie))
+                appendMovie(movie, bodyElement)
             }
         } else {
             bodyElement.append("Daten konnten nicht geladen werden, Status " + xhr.status + " - " + xhr.statusText)
